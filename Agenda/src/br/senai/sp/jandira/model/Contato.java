200f6541c7f27268c17b122fab026a403fa84309
17259224 <17259224@DSN1271058229.cfp127aluno.local>
2018-04-05 10:26:27 -0300

    modificação 3

--- Agenda/src/br/senai/sp/jandira/model/Contato.java
+++ Agenda/src/br/senai/sp/jandira/model/Contato.java
@@ -4,7 +4,52 @@ public class Contato {
 	private int id;
 	private String nome;
 	private String celular;
+	private String telefone;
 	private String email;
 	private String dtNascimento;
 	private String endereco;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDtNascimento() {
+		return dtNascimento;
+	}
+	public void setDtNascimento(String dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
 }
