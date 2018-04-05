e502f3fb81ae6cef24ea24c9f704bf0ca8600806
17259224 <17259224@DSN1271058229.cfp127aluno.local>
2018-04-05 10:07:23 -0300

    Acrescimo dos atributos da classe contato

--- Agenda/src/br/senai/sp/jandira/model/Contato.java
+++ Agenda/src/br/senai/sp/jandira/model/Contato.java
@@ -1,5 +1,10 @@
 package br.senai.sp.jandira.model;
 
 public class Contato {
-
+	private int id;
+	private String nome;
+	private String celular;
+	private String email;
+	private String dtNascimento;
+	private String endereco;
 }
